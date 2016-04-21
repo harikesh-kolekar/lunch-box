8d60dd97175ad6d071c11d19b8c722a92cd3bb98
Harikesh Kolekar <harikesh.kolekar@harbingergroup.com>
2016-04-21 18:48:42 +0530

    first commit

---
+++ www/js/common/app.js
@@ -0,0 +1,25 @@
+app.constant("Config", {
+  "ApiUrl": "data/feed.json",
+  "PhotoUrl": "data/photos.json",
+  "CommentUrl": "data/comments.json",
+  "FriendsUrl": "data/friends.json",
+  "MessagesUrl": "data/messages.json",
+  "MessageUrl": "data/message.json",
+  "ProductUrl": "data/products.json",
+  "WordPress": "http://data.dearpins.com/wp/",
+  "ResendAuthenticationEmailUrl": "api/device/resend_authentication_email",
+})
+// config contact
+app.constant("ConfigContact", {
+  "EmailId": "weblogtemplatesnet@gmail.com",
+  "ContactSubject": "Contact"
+})
+// config admon
+app.constant("ConfigAdmob", {
+  "interstitial": "ca-app-pub-3940256099942544/1033173712",
+  "banner": "ca-app-pub-3940256099942544/6300978111"
+})
+// push notification
+app.constant("PushNoti", {
+  "senderID": "321948427526",
+})
